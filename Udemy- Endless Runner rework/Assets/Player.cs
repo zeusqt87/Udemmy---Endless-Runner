0f29bb7a0dc74e016155eb71cd1ee172682a6e24
zeusqt87 <69776440+zeusqt87@users.noreply.github.com>
2023-07-16 22:38:57 +0300

    Idle animation

--- Udemy- Endless Runner rework/Assets/Player.cs
+++ Udemy- Endless Runner rework/Assets/Player.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public bool runBegun;
-    public float moveSpeed;
-    public float jumpForce;
+    private Rigidbody2D rb;
+    private Animator anim;
 
-    public Rigidbody2D rb;
+    [Header("Move Info")]
+    [SerializeField]private float moveSpeed;
+    [SerializeField]private float jumpForce;
+
+    private bool runBegun;
+
+    [Header("Collision Info")]
+   [SerializeField] private float groundCheckDistance;
+   [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
 
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
@@ -25,6 +35,8 @@ public class Player : MonoBehaviour
         {
             rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
         }
+
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
 
         CheckInput();
 
@@ -37,10 +49,14 @@ public class Player : MonoBehaviour
             runBegun = true;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)&& isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
 
         }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
