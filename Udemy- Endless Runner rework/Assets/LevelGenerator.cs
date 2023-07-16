cc63fcd329ba18add2061709252fe66dc23d29a5
zeusqt87 <69776440+zeusqt87@users.noreply.github.com>
2023-07-17 00:25:26 +0300

    Started platform generation
    need to work on unlimited platform generation

---
+++ Udemy- Endless Runner rework/Assets/LevelGenerator.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField]private Transform platform;
+    [SerializeField] private Transform respawnPosition;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
