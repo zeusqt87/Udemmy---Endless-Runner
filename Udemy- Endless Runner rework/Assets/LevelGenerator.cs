8ae887d4aa9d0b3b496338a21fa378e98ffbf7c9
zeusqt87 <69776440+zeusqt87@users.noreply.github.com>
2023-07-17 16:00:01 +0300

    Endless genneration
    platforms generates and deletes automatically

--- Udemy- Endless Runner rework/Assets/LevelGenerator.cs
+++ Udemy- Endless Runner rework/Assets/LevelGenerator.cs
@@ -4,14 +4,44 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    [SerializeField]private Transform platform;
-    [SerializeField] private Transform respawnPosition;
+    [SerializeField]private Transform[] levelPart;
+    [SerializeField] private Vector3 nextPartPosition;
+
+    [SerializeField] private float distanceToSpawn;
+    [SerializeField] private float distanceToDelete;
+    [SerializeField] private Transform player;
 
 
 
     // Update is called once per frame
     void Update()
     {
-        
+        DeletePlatform();
+        GeneratePlatform();
+    }
+
+    private void GeneratePlatform()
+    {
+        while (Vector2.Distance(player.transform.position,nextPartPosition)< distanceToSpawn)
+        {
+            Transform part = levelPart[Random.Range(0, levelPart.Length)];
+
+            Vector2 newPosition = new Vector2(nextPartPosition.x - part.Find("StartPoint").position.x, 0);
+
+            Transform newPart = Instantiate(part, newPosition, transform.rotation, transform);
+
+            nextPartPosition = newPart.Find("EndPoint").position;
+
+        }
+    }
+    private void DeletePlatform()
+    {
+        if (transform.childCount > 0)
+        {
+            Transform partToDelete = transform.GetChild(0);
+
+            if (Vector2.Distance(player.transform.position, partToDelete.transform.position) > distanceToDelete)
+                Destroy(partToDelete.gameObject);
+        }
     }
 }
